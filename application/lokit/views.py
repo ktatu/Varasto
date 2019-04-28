68d40f28db17ef4a54bdecb96fb258fb75e2d2f4
ktatu <kaikkonentatu@gmail.com>
2019-04-29 02:21:47 +0300

    Sivutus loki-sivuilla

--- application/lokit/views.py
+++ application/lokit/views.py
@@ -1,26 +1,44 @@
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, request
 from flask_login import login_required, current_user
+from sqlalchemy import desc
 
 from application import app, db
 from application.lokit.models import Loki
 from application.hyllypaikat.models import Hyllypaikka
 
- # kirjautuneen käyttäjän lokit
+ # kirjautuneen käyttäjän lokit, sisältää sivutuksen
 @app.route("/lokit/userlogs")
 @login_required
 def kayttaja_lokit():
+    sivu = request.args.get('sivu', 1, type=int)
+    lokit = Loki.query.filter(Loki.account_id == current_user.id).order_by(Loki.luotu.desc()).paginate(sivu, 12, False)
 
-    return render_template("/lokit/userlogs.html", lokit = current_user.lokit)
+    edellinen_sivu = url_for('kayttaja_lokit', sivu = lokit.prev_num) \
+        if lokit.has_prev else None
+
+    seuraava_sivu = url_for('kayttaja_lokit', sivu = lokit.next_num) \
+        if lokit.has_next else None
+
+    return render_template("/lokit/userlogs.html", lokit = lokit.items, edellinen_sivu = edellinen_sivu, seuraava_sivu = seuraava_sivu)
 
  # hyllypaikan lokit
 @app.route("/lokit/<paikkanumero>")
 @login_required
 def hyllypaikka_lokit(paikkanumero):
 
-    hyllypaikka = Hyllypaikka.query.filter(Hyllypaikka.paikkanumero == paikkanumero).first()
+#    hyllypaikka = Hyllypaikka.query.filter(Hyllypaikka.paikkanumero == paikkanumero).first()
 
-    if hyllypaikka:
-        return render_template("/lokit/shelflogs.html", lokit = hyllypaikka.lokit)
+    sivu = request.args.get('sivu', 1, type=int)
+    lokit = Loki.query.filter(Loki.paikkanumero == paikkanumero).order_by(Loki.luotu.desc()).paginate(sivu, 12, False)
+
+    edellinen_sivu = url_for('hyllypaikka_lokit', sivu = lokit.prev_num) \
+        if lokit.has_prev else None
+
+    seuraava_sivu = url_for('hyllypaikka_lokit', sivu = lokit.next_num) \
+        if lokit.has_next else None
+
+    if lokit:
+        return render_template("/lokit/shelflogs.html", lokit = lokit.items, edellinen_sivu = edellinen_sivu, seuraava_sivu = seuraava_sivu)
 
     # pitäisi tapahtua ainoastaan jos kirjoittaa url-bariin hyllypaikan, johon ei ikinä ole hyllytetty
     else:
